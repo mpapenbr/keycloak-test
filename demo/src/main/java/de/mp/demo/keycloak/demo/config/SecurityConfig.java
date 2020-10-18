d00caa8eae5e4b48546c23b3245a700795807a95
mpapenbr <Markus.Papenbrock@gmail.com>
2020-10-18 09:05:21 +0200

    first try (not working)

--- demo/src/main/java/de/mp/demo/keycloak/demo/config/SecurityConfig.java
+++ demo/src/main/java/de/mp/demo/keycloak/demo/config/SecurityConfig.java
@@ -3,34 +3,23 @@ package de.mp.demo.keycloak.demo.config;
 import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
 import org.keycloak.adapters.springsecurity.KeycloakConfiguration;
 import org.keycloak.adapters.springsecurity.authentication.KeycloakAuthenticationProvider;
-import org.keycloak.adapters.springsecurity.client.KeycloakClientRequestFactory;
-import org.keycloak.adapters.springsecurity.client.KeycloakRestTemplate;
 import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Scope;
-import org.springframework.http.client.ClientHttpRequestFactory;
-import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.authority.mapping.SimpleAuthorityMapper;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.session.SessionRegistryImpl;
 import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
 import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
 
 //@Configuration
 @KeycloakConfiguration
-
+@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true, jsr250Enabled = true)
 public class SecurityConfig extends KeycloakWebSecurityConfigurerAdapter {
-    private KeycloakClientRequestFactory keycloakClientRequestFactory;
 
-    public SecurityConfig(KeycloakClientRequestFactory keycloakClientRequestFactory) {
-        this.keycloakClientRequestFactory = keycloakClientRequestFactory;
-
-        // to use principal and authentication together with @async
-        SecurityContextHolder.setStrategyName(SecurityContextHolder.MODE_INHERITABLETHREADLOCAL);
+    public SecurityConfig() {
 
     }
 
@@ -48,17 +37,6 @@ public class SecurityConfig extends KeycloakWebSecurityConfigurerAdapter {
     }
 
     @Bean
-    public ClientHttpRequestFactory clientHttpRequestFactory() {
-        return new SimpleClientHttpRequestFactory();
-    }
-
-    @Bean
-    @Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
-    public KeycloakRestTemplate keycloakRestTemplate() {
-        return new KeycloakRestTemplate(keycloakClientRequestFactory);
-    }
-
-    @Bean
     @Override
     protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
 
@@ -73,12 +51,11 @@ public class SecurityConfig extends KeycloakWebSecurityConfigurerAdapter {
                 // .cors(c -> {
                 // }).csrf(c -> c.disable())
 
-                // .sessionManagement(s ->
-                // s.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                // .sessionManagement(s -> s
+                // .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 // .sessionAuthenticationStrategy(sessionAuthenticationStrategy()))
-                .authorizeRequests(a -> a.antMatchers("/hello/user*").hasRole("user").antMatchers("/hello/user/*")
-                        .hasRole("user").anyRequest().permitAll());
+                .authorizeRequests(a -> a.antMatchers("/hello/user").authenticated().antMatchers("/hello/user/*")
+                        .authenticated().anyRequest().permitAll());
     }
 
-    
 }
